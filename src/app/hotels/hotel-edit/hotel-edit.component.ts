ffa5e5f6e34ecf5dbe3048fe0bec5505cf5d31ff
rachAbderr <abderrahmane.rachid10@gmail.com>
2021-09-23 14:16:56 +0200

    complete saveHotel method & create creatHotel method

--- src/app/hotels/hotel-edit/hotel-edit.component.ts
+++ src/app/hotels/hotel-edit/hotel-edit.component.ts
@@ -1,57 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IHotel } from '../shared/models/hotel';
 import { HotelListService } from '../shared/services/hotel-list.service';
 
 @Component({
   selector: 'app-hotel-edit',
   templateUrl: './hotel-edit.component.html',
-  styleUrls: ['./hotel-edit.component.css']
+  styleUrls: ['./hotel-edit.component.css'],
 })
 export class HotelEditComponent implements OnInit {
-
-  public hotelForm : FormGroup;
-  public hotel : IHotel;
+  public hotelForm: FormGroup;
+  public hotel: IHotel;
   public pageTitle: string;
 
   constructor(
-    private fb : FormBuilder, private route : ActivatedRoute, private hotelListService : HotelListService) { }
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private hotelListService: HotelListService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.hotelForm = this.fb.group({
-        hotelName :['',Validators.required],
-        hotelPrice :['', Validators.required],
-        starRating : [''],
-        description : ['']
+      hotelName: ['', Validators.required],
+      price: ['', Validators.required],
+      rating: [''],
+      description: [''],
     });
 
-    this.route.paramMap.subscribe(params =>{
+    this.route.paramMap.subscribe((params) => {
       const id = +params.get('id');
-      
+
       this.getSelectedHotel(id);
-    })
+    });
   }
- 
-  public getSelectedHotel(id: number):void{
-    this.hotelListService.getHotelById(id).subscribe(
-      (hotel:IHotel)=>this.displayHotel(hotel));
+
+  public getSelectedHotel(id: number): void {
+    this.hotelListService
+      .getHotelById(id)
+      .subscribe((hotel: IHotel) => this.displayHotel(hotel));
   }
-  public displayHotel(hotel : IHotel): void{
+  public displayHotel(hotel: IHotel): void {
     this.hotel = hotel;
-    if(this.hotel.id == 0){
-      this.pageTitle = 'Créer un hotel'; 
-    }else{
-      this.pageTitle = `Modifier l\'hotel ${this.hotel.hotelName}`
+    if (this.hotel.id == 0) {
+      this.pageTitle = 'Créer un hotel';
+    } else {
+      this.pageTitle = `Modifier l\'hotel ${this.hotel.hotelName}`;
     }
     this.hotelForm.patchValue({
-      hotelName : this.hotel.hotelName,
-      hotelPrice : this.hotel.price,
-      starRating : this.hotel.rating,
-      description : this.hotel.description 
+      hotelName: this.hotel.hotelName,
+      price: this.hotel.price,
+      rating: this.hotel.rating,
+      description: this.hotel.description,
+    });
+  }
+  saveHotel() {
+    if (this.hotelForm.valid) {    
+      if (this.hotelForm.dirty) {
+        const hotel: IHotel = {
+          ...this.hotel,
+          ...this.hotelForm.value,
+        };
+        if (hotel.id == 0) {
+          this.hotelListService
+            .creatHotel(hotel)
+            .subscribe({ next: () => this.saveCompleted() });
+        } else {
+          this.hotelListService.updateHotel(hotel).subscribe({
+            next: () => this.saveCompleted(),
+          });
+        }
+      }
     }
-  )};
-  saveHotel(){
     console.log(this.hotelForm.value);
+  }
+  public saveCompleted(): void {
+    this.hotelForm.reset();
+    this.router.navigate(['/hotels']);
   }
 }
--- src/app/hotels/shared/services/hotel-list.service.ts
+++ src/app/hotels/shared/services/hotel-list.service.ts
@@ -2,48 +2,55 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { IHotel } from '../models/hotel';
-import {catchError, tap, map} from 'rxjs/operators'
+import { catchError, tap, map } from 'rxjs/operators';
+//import { url } from 'inspector';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HotelListService {
-
   private readonly HOTEL_API_URL = 'api/hotels';
 
-  constructor(private http : HttpClient) { }
- 
+  constructor(private http: HttpClient) {}
 
-  public getHotels():Observable<IHotel[]>{
-    return this.http.get<IHotel[]>(this.HOTEL_API_URL)
-                    .pipe(tap(hotels=>console.log('hotels :',hotels)),
-                    catchError(this.handleError));                 
+  public getHotels(): Observable<IHotel[]> {
+    return this.http.get<IHotel[]>(this.HOTEL_API_URL).pipe(
+      tap((hotels) => console.log('hotels :', hotels)),
+      catchError(this.handleError)
+    );
+  }
+
+  public getHotelById(id: number): Observable<IHotel> {
+    const url = `${this.HOTEL_API_URL}/${id}`;
+    if (id == 0) {
+      return of(this.getDefaultHotel());
     }
-    
-  public getHotelById( id : number):Observable<IHotel>{
-      if(id == 0){
-        return of(this.getDefaultHotel());
-      }
-      return this.getHotels().pipe(
-        map(hotels=>hotels.find(hotel => hotel.id == id))  
-      );
-  }  
+    return this.http.get<IHotel>(url).pipe(catchError(this.handleError));
+  }
 
-  public updateHotel(hotel: IHotel): Observable<IHotel>{
+  public updateHotel(hotel: IHotel): Observable<IHotel> {
     const url = `${this.HOTEL_API_URL}/${hotel.id}`;
 
-    return this.http.put<IHotel>(url,hotel).pipe(catchError(this.handleError));
+    return this.http.put<IHotel>(url, hotel).pipe(catchError(this.handleError));
   }
-  private getDefaultHotel():IHotel{
+  
+  public creatHotel(hotel: IHotel): Observable<IHotel> {
+    return this.http
+      .post<IHotel>(this.HOTEL_API_URL, hotel)
+      .pipe(catchError(this.handleError));
+  }
+
+  private getDefaultHotel(): IHotel {
     return {
-    id: 0,
-    hotelName: null,
-    description: null ,
-    price: null ,
-    imageUrl: null,
-    rating: null, 
-    }
+      id: 0,
+      hotelName: null,
+      description: null,
+      price: null,
+      imageUrl: null,
+      rating: null,
+    };
   }
+
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
@@ -52,13 +59,11 @@ export class HotelListService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
     }
     // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.');
+    return throwError('Something bad happened; please try again later.');
   }
-
 }
-
-
